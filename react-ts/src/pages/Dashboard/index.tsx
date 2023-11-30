c18381d941358e00093d2f594fe10bee34df80e6
Samson Njuguna <96062644+samnjuguna44@users.noreply.github.com>
2023-11-30 12:53:44 +0300

    completed dashboard component

--- react-ts/src/pages/Dashboard/index.tsx
+++ react-ts/src/pages/Dashboard/index.tsx
@@ -1,21 +1,137 @@
-import React from "react";
-import { Typography, Space, Card, Statistic } from "antd";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { Typography, Space, Card, Statistic, Table } from "antd";
 import {
   ShoppingCartOutlined,
   DollarCircleOutlined,
   UserOutlined,
   ShoppingOutlined,
 } from "@ant-design/icons";
+import { getOrders, getRevenue } from "../../API";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 interface DashboardCardProps {
   title: string;
   value: number;
-  icon: React.ReactNode;
+  icon: JSX.Element;
+}
+
+function DashboardCard({ title, value, icon }: DashboardCardProps) {
+  return (
+    <Card>
+      <Space direction="horizontal">
+        {icon}
+        <Statistic title={title} value={value} />
+      </Space>
+    </Card>
+  );
+}
+
+function RecentOrders() {
+  const [dataSource, setDataSource] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    getOrders().then((res) => {
+      setDataSource(res.products.slice(0, 3));
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <>
+      <Typography.Text>Recent Orders</Typography.Text>
+      <Table
+        columns={[
+          {
+            title: "Title",
+            dataIndex: "title",
+          },
+          {
+            title: "Quantity",
+            dataIndex: "quantity",
+          },
+          {
+            title: "Price",
+            dataIndex: "discountedPrice",
+          },
+        ]}
+        loading={loading}
+        dataSource={dataSource}
+        pagination={false}
+      ></Table>
+    </>
+  );
+}
+
+function DashBoardChart() {
+  const [revenueData, setRevenueData] = useState({
+    labels: [] as string[],
+    datasets: [] as any[],
+  });
+
+  useEffect(() => {
+    getRevenue().then((res) => {
+      const labels = res.carts.map((cart) => `User-${cart.userId}`);
+      const data = res.carts.map((cart) => cart.discountedTotal);
+
+      const dataSource = {
+        labels,
+        datasets: [
+          {
+            label: "Revenue",
+            data,
+            backgroundColor: "rgb(204, 85, 0)",
+          },
+        ],
+      };
+
+      setRevenueData(dataSource);
+    });
+  }, []);
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Order Revenue",
+      },
+    },
+  };
+
+  return (
+    <Card style={{ width: 500, height: 250 }}>
+      <Bar options={options} data={revenueData} />;
+    </Card>
+  );
 }
 
 function Dashboard() {
   return (
-    <div>
+    <Space size={20} direction="vertical">
       <Typography.Title level={4}>Dashboard</Typography.Title>
       <Space direction="horizontal">
         <DashboardCard
@@ -79,18 +195,11 @@ function Dashboard() {
           value={14670}
         />
       </Space>
-    </div>
-  );
-}
-
-function DashboardCard({ title, value, icon }: DashboardCardProps) {
-  return (
-    <Card>
-      <Space direction="horizontal">
-        {icon}
-        <Statistic title={title} value={value} />
+      <Space>
+        <RecentOrders />
+        <DashBoardChart />
       </Space>
-    </Card>
+    </Space>
   );
 }
 
